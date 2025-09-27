f57277e82323190f9808c7c9b66fc221e9217d78
Bkoltz <fstorm2080@gmail.com>
2025-09-27 18:36:12 -0500

    Fixed database timeout error. Issue caused by SSl Cert.

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,6 +1,6 @@
 <main>
   <section>
     <h2>Your Drone Media Portal</h2>
-    <p>Access quotes, contracts, and invoices securely.</p>
+    <p>Access quotes, contracts, and invoices securely. test</p>
   </section>
 </main>
