74c350b1d85eaefb218f4ceed7df94c6582ba76f
bkoltz <fstorm2080@gmail.com>
2026-07-02 16:53:48 -0500

    Make org link strategy migration idempotent

--- database/migrations/0006_org_link_strategy_and_link_only_onboarding.sql
+++ database/migrations/0006_org_link_strategy_and_link_only_onboarding.sql
@@ -1,6 +1,20 @@
-ALTER TABLE organizations
-    ADD COLUMN link_strategy ENUM('department_links_only','overall_folder','shared_folder') NOT NULL DEFAULT 'department_links_only'
-    AFTER tax_exempt_uploaded_at;
+SET @org_link_strategy_exists := (
+    SELECT COUNT(*)
+    FROM information_schema.columns
+    WHERE table_schema = DATABASE()
+      AND table_name = 'organizations'
+      AND column_name = 'link_strategy'
+);
+
+SET @org_link_strategy_sql := IF(
+    @org_link_strategy_exists = 0,
+    'ALTER TABLE organizations ADD COLUMN link_strategy ENUM(''department_links_only'',''overall_folder'',''shared_folder'') NOT NULL DEFAULT ''department_links_only'' AFTER tax_exempt_uploaded_at',
+    'SELECT 1'
+);
+
+PREPARE org_link_strategy_stmt FROM @org_link_strategy_sql;
+EXECUTE org_link_strategy_stmt;
+DEALLOCATE PREPARE org_link_strategy_stmt;
 
 ALTER TABLE client_onboarding_invitations
     MODIFY invited_email VARCHAR(255) NULL;
